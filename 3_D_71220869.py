bfa004ebbdc49420fd52afb02ad10221c7ce4436
Nicholandn22 <117884361+Nicholandn22@users.noreply.github.com>
2023-02-22 18:41:49 +0700

    Add files via upload

--- 3_D_71220869.py
+++ 3_D_71220869.py
@@ -1,8 +1,7 @@
 a = int(input('Masukkan awal deret: '))
 b = int(input('Masukkan akhir deret: '))
-c = '  '
-d = [' ']
+d = ['']
 while a <= b:
     None if (a % 6) == 0 or (a % 8) == 0 else d.append(str(a))
     a = a + 1
-print (c.join(d))
+print (('  ').join(d))
